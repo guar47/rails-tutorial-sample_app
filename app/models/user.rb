c4722a83f0734393f8bf1d06e555524483cf8f5f
Dmitrii Pashutskii <dpashutskii@gmail.com>
2018-09-08 16:45:13 +0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
   before_save {self.email = email.downcase}
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true,
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def self.digest(string)
